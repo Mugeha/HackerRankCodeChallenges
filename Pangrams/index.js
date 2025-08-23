9ac38c1caf55adb39d5f122ea9ad23a73d71182e
Mugeha <mugehajacky@gmail.com>
2025-08-23 11:14:25 +0300

    Pangrams

---
+++ Pangrams/index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'pangrams' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+function pangrams(s) {
+    // Convert to lowercase and put each char into a Set
+    const letters = new Set(s.toLowerCase().replace(/[^a-z]/g, ''));
+
+    // If we have 26 unique letters -> it's a pangram
+    return letters.size === 26 ? "pangram" : "not pangram";
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    const result = pangrams(s);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
