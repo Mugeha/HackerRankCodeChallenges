d17377653a096e64f387e65489b3786a716b96d0
Mugeha <mugehajacky@gmail.com>
2025-10-03 17:11:28 +0300

    Gaming array I

---
+++ Gaming Array/index.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'gamingArray' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function gamingArray(arr) {
+    let maxSoFar = -Infinity;
+    let moves = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > maxSoFar) {
+            maxSoFar = arr[i];
+            moves++;
+        }
+    }
+
+    if (moves % 2 === 1) {
+        return "BOB";
+    } else {
+        return "ANDY";
+    }
+}
+
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const g = parseInt(readLine().trim(), 10);
+
+    for (let gItr = 0; gItr < g; gItr++) {
+        const arrCount = parseInt(readLine().trim(), 10);
+
+        const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+        const result = gamingArray(arr);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
